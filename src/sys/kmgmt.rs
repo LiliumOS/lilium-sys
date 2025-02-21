fed70431fda646e99b7867034f472d5784b7002b
Connor Horman <chorman64@gmail.com>
2025-02-21 16:46:30 -0500

    expect(improper_ctypes)

--- src/sys/kmgmt.rs
+++ src/sys/kmgmt.rs
@@ -32,6 +32,7 @@ pub union KernelCfgOption {
 }
 
 #[cfg(any(feature = "kmgmt", doc))]
+#[expect(improper_ctypes)]
 unsafe extern "system" {
     pub unsafe fn ConfigureKernel(cfg: KCSlice<KernelCfgOption>) -> SysResult;
     /// Opens a kernel module specified by `path`. If `path` is relative, it is resolved by `res_base` or the
